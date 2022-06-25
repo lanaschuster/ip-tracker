3cc39625226089f7c7dfc079e51f320fde88c6a5
Lana Schuster <lanaschuster1@gmail.com>
2022-06-25 20:02:16 -0300

    feat: search result card

---
+++ components/utils/CardContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card-container {
+  background: $color-light;
+  border-radius: $border-radius;
+  height: fit-content;
+  width: fit-content;
+}
+</style>
